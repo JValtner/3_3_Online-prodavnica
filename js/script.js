2fd57eb9559e16315490227a7714ace6d5610eb4
Josip Valtner <josipvaltner@gmail.com>
2025-04-29 15:58:11 +0200

    Dodat style.css i definisane klase konstruktori i niz artikala

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 `use strict`
+class Artikal{
+    construktor(jedinstveniBroj, naziv, cena, dostupnost){
+        this.jedinstveniBroj = jedinstveniBroj
+        this.naziv = naziv
+        this.cena = cena
+        this.dostupnost = dostupnost
+    }
+}
+let artMonitor = new Artikal("Monitor", 165, true)
+let artTv = new Artikal("TV", 650, false)
+let artMis = new Artikal("Mis", 20, true)
+let artTastatura = new Artikal("Tastatura", 55,false)
+const artikli =[artMonitor,artTv,artMis,artTastatura]
